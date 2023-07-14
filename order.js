c2d263153305bd56bf1a4b2307cef72fc15b4a52
kaytl <kaytl@DESKTOP-TI4LR7A>
2023-07-14 19:52:12 +0100

    finished :)

--- order.js
+++ order.js
@@ -1,9 +1,35 @@
 window.addEventListener("DOMContentLoaded", function(e){
 
+    const locationBox = document.querySelector("#user-location")
+
+    let location = {
+        latitude : "unknown",
+        longitude : "unknown"
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+
+        function(position){
+            location = {
+                latitude : position.coords.latitude,
+                longitude : position.coords.longitude
+            };
+            locationBox.value = json.stringify(location);
+        },
+    
+        function(error){
+            console.log(`Error getting user location. '${error.message}'`)
+            locationBox.value = JSON.stringify(location);
+        }
+    );
+
     const order = this.localStorage.getItem("order");
 
         if (order) { //by passing 'order' as the if expression, java is asking if the order variable has 'truthiness' to it, this checks if it has a value and does not default to null
             const pieOrder = JSON.parse(order);
+
+            const orderInput = document.querySelector("#pie-order")
+            orderInput.value = order;
 
             const pie = this.document.querySelector(".pie");
 
@@ -20,3 +46,4 @@ window.addEventListener("DOMContentLoaded", function(e){
             img.setAttribute("alt", pieOrder.title)
         }
 });
+
